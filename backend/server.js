9c0dde67b8227170b9218910f05fe553ab0608d4
Mohd Imran <mohd78696321@gmail.com>
2023-04-18 14:31:43 +0530

    production code

--- backend/server.js
+++ backend/server.js
@@ -32,10 +32,6 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (_, res) => {
-  res.status(200).json({ message: "Welcome to the Pizza Mania API" });
-});
-
 // Routes
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/products", require("./routes/productRoutes"));
@@ -73,6 +69,21 @@ app.post("/razorpay", async (req, res) => {
     console.log(error);
   }
 });
+
+// Serve Frontend
+if (process.env.NODE_ENV === "production") {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  // FIX: below code fixes app crashing on refresh in deployment
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  });
+} else {
+  app.get("/", (_, res) => {
+    res.status(200).json({ message: "Welcome to the Pizza Mania API" });
+  });
+}
 
 app.use(errorHandler);
 
